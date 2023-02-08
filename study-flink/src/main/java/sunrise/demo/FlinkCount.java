2ece0d5c81f60d9f28eea1ded401726e888ef90d
wqkenqing <kuiqwang@ken.local>
2023-02-08 17:14:27 +0800

    [dev]更新

--- study-flink/src/main/java/sunrise/demo/FlinkCount.java
+++ study-flink/src/main/java/sunrise/demo/FlinkCount.java
@@ -1,5 +1,6 @@
 package sunrise.demo;
 
+import org.apache.flink.api.common.functions.RichReduceFunction;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
@@ -16,10 +17,8 @@ public class FlinkCount {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         DataStreamSource<String> textSource = env.readTextFile("/Users/kuiqwang/Desktop/tengxun_class/study-flink/src/main/resources/carFlow_all_column_test.txt");
-        textSource.map(s -> Tuple2.of("record", 1)).returns(Types.TUPLE(Types.STRING, Types.INT))
-                .keyBy(0).
-                countWindowAll(1000)
-                .sum(1).print();
+        textSource.map(s -> Tuple2.of(s.split(",")[2], 1)).returns(Types.TUPLE(Types.STRING,Types.INT)).keyBy(0).sum(1).filter(s -> Integer.valueOf(s.f1) > 2).print();
+
         env.execute();
     }
 
